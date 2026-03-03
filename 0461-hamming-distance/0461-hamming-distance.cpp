d593754529d01e1ba62b197e82c9e12de0c6ee11
reeganferdinand <reeganferdinand.k0710@gmail.com>
2026-03-03 22:01:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (67.25%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int num=x^y;
+
+        int count=0;
+
+        while(num>0)
+        {
+            num=num&(num-1);
+            count++;
+        }
+
+        return count;
+    }
+};
