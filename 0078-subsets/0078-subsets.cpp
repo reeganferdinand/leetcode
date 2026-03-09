1a3c7111d7c80801420aca5d55fe2f26272f917e
reeganferdinand <reeganferdinand.k0710@gmail.com>
2026-03-09 19:47:50 +0530

    Time: 3 ms (29.92%), Space: 9.9 MB (60.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-
-    void func(vector<vector<int>> &ans, int ind, vector<int>& nums, 
-              vector<int>& arr, int n)
-    {
-        if(ind >= n)
-        {
-            ans.push_back(arr);
-            return;
-        }
-
-        // Take element
-        arr.push_back(nums[ind]);
-        func(ans, ind+1, nums, arr, n);
-
-        // Not take element
-        arr.pop_back();
-        func(ans, ind+1, nums, arr, n);
-    }    
     
     vector<vector<int>> subsets(vector<int>& nums) {
        
        int n = nums.size();   
        vector<vector<int>> ans;
-       vector<int> arr;
+       
+       int mask;
 
-       func(ans, 0, nums, arr, n);
+       for(mask=0;mask<(1<<n);mask++)
+       {
+            vector<int> arr;
+            for(int i=0;i<n;i++)
+            {
+                if(mask&(1<<i))
+                {
+                    arr.push_back(nums[i]);
+                }
+            }
+
+            ans.push_back(arr);
+       }
+
 
        return ans;
     }
