2b601a3d0144040bf6ed63badbb65e1001a20c1c
reeganferdinand <reeganferdinand.k0710@gmail.com>
2026-07-09 20:56:51 +0530

    Time: 0 ms (100%), Space: 77.1 MB (45.39%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -7,26 +7,27 @@ public:
 
         int carry = 0;
 
-        while (l1 || l2 || carry) {
+       while(l1 || l2 || carry)
+       {
+            int sum=carry;
 
-            int sum = carry;
-
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;
+            if(l1!=nullptr)
+            {
+                sum+=l1->val;
+                l1=l1->next;
             }
 
-            if (l2) {
-                sum += l2->val;
-                l2 = l2->next;
+            if(l2!=nullptr)
+            {
+                sum+=l2->val;
+                l2=l2->next;
             }
 
-            carry = sum / 10;
+            temp->next=new ListNode(sum%10);
+            temp=temp->next;
+            carry=sum/10;
+       }
 
-            temp->next = new ListNode(sum % 10);
-            temp = temp->next;
-        }
-
-        return dummy->next;
+       return dummy->next;
     }
 };
