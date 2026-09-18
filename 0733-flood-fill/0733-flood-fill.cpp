7cf72696515c17015e5902f9910d438e90cc23f9
reeganferdinand <reeganferdinand.k0710@gmail.com>
2026-09-18 22:16:12 +0530

    Time: 0 ms (100%), Space: 18.5 MB (15.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    
-    void dfs(int r,int c,int clr,vector<vector<int>> &grid,vector<vector<int>>&vis)
+
+    void dfs(int r, int c, vector<vector<int>> &image,
+             int clr, int original, int n, int m)
     {
-        int n=grid.size();
-        int m=grid[0].size();
-        int org=grid[r][c];
-        vis[r][c]=1;
-        grid[r][c]=clr;
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
+        image[r][c] = clr;
 
-        for(int i=0;i<4;i++)
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+
+        for(int i = 0; i < 4; i++)
         {
-            int nr=r+dr[i];
-            int nc=c+dc[i];
+            int nr = r + dr[i];
+            int nc = c + dc[i];
 
-            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==org && !vis[nr][nc])
-            {
-                dfs(nr,nc,clr,grid,vis);
-            }
+            if(nr < 0 || nc < 0 || nr >= n || nc >= m)
+                continue;
+
+            if(image[nr][nc] != original)
+                continue;
+
+            dfs(nr, nc, image, clr, original, n, m);
         }
     }
-    
-    
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
 
-        if(image[sr][sc]==color) return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& image,
+                                   int sr, int sc, int color)
+    {
+        if(image.empty())
+            return image;
 
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        dfs(sr,sc,color,image,vis);
+        int n = image.size();
+        int m = image[0].size();
+
+        int original = image[sr][sc];
+
+        // If both colors are same, nothing to do
+        if(original == color)
+            return image;
+
+        dfs(sr, sc, image, color, original, n, m);
 
         return image;
-
     }
 };
