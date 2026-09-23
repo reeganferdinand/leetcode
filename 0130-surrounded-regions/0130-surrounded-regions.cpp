ca6ef93359dc04fe80274b1c7469e51cb43dadb9
reeganferdinand <reeganferdinand.k0710@gmail.com>
2026-09-23 22:53:30 +0530

    Time: 4 ms (12.48%), Space: 14.7 MB (34.81%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -3,73 +3,63 @@ public:
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(n,vector<int>(m,0));
+
 
         vector<vector<bool>> can(n,vector<bool>(m,true));
 
-        // Top row
-for (int j = 0; j < m; j++)
-{
-    if (board[0][j] == 'O' && !vis[0][j])
-    {
-        q.push({0, j});
-        vis[0][j] = 1;
-        can[0][j] = false;
-    }
-}
+        queue<pair<int,int>> q;
 
-// Bottom row
-for (int j = 0; j < m; j++)
-{
-    if (board[n - 1][j] == 'O' && !vis[n - 1][j])
-    {
-        q.push({n - 1, j});
-        vis[n - 1][j] = 1;
-        can[n - 1][j] = false;
-    }
-}
+        // rows
+        for(int i=0;i<n;i++)
+        {
+            if(board[i][0]=='O')
+            {
+                can[i][0]=false;
+                q.push({i,0});
+            }
 
-// Left column
-for (int i = 1; i < n - 1; i++)
-{
-    if (board[i][0] == 'O' && !vis[i][0])
-    {
-        q.push({i, 0});
-        vis[i][0] = 1;
-        can[i][0] = false;
-    }
-}
+            if(board[i][m-1]=='O')
+            {
+                can[i][m-1]=false;
+                q.push({i,m-1});
+            }
 
-// Right column
-for (int i = 1; i < n - 1; i++)
-{
-    if (board[i][m - 1] == 'O' && !vis[i][m - 1])
-    {
-        q.push({i, m - 1});
-        vis[i][m - 1] = 1;
-        can[i][m - 1] = false;
-    }
-}
+        }
+
+        // colums
+
+        for(int i=0;i<m;i++)
+        {
+            if(board[0][i]=='O')
+            {
+                can[0][i]=false;
+                q.push({0,i});
+            }
+
+            if(board[n-1][i]=='O')
+            {
+                can[n-1][i]=false;
+                q.push({n-1,i});
+            }
+        }
+
         int dr[]={-1,0,1,0};
         int dc[]={0,1,0,-1};
 
         while(!q.empty())
         {
-            auto it=q.front();
+            int r=q.front().first;
+            int c=q.front().second;
             q.pop();
 
-            int r=it.first;
-            int c=it.second;
 
             for(int i=0;i<4;i++)
             {
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                
-                if(nr>=0 && nr<n && nc>=0 && nc<m && board[nr][nc]=='O' && !vis[nr][nc])
+
+                if(nr>=0 && nc>=0 && nr<n && nc<m && board[nr][nc]=='O'&& can[nr][nc])
                 {
-                    vis[nr][nc]=1;
                     can[nr][nc]=false;
                     q.push({nr,nc});
                 }
@@ -80,11 +70,13 @@ for (int i = 1; i < n - 1; i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j]=='O' && can[i][j])
+                if(can[i][j]==true)
                 {
                     board[i][j]='X';
                 }
             }
         }
+
+
     }
 };
